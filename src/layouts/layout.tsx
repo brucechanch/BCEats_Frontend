b7d8cf00fb81a3f9bdd771672f538b957b820ab8
brucechanch <brucechanch@gmail.com>
2024-04-25 14:32:34 +0800

    first Comment

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,21 @@
+import Footer from '@/components/Footer'
+import Hero from '@/components/Hero'
+import Header from '@/components/header'
+
+type Props = {
+  children: React.ReactNode
+  showHero?: boolean
+}
+
+const layout = ({ children, showHero = false }: Props) => {
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      {showHero && <Hero />}
+      <div className='container mx-auto flex-1 py-10'>{children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default layout
