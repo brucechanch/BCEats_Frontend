e17f5ba98627b2d80a894b0e125ebabbbb504544
brucechanch <brucechanch@gmail.com>
2024-04-29 17:19:45 +0800

    bug fixed

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,7 +5,6 @@ import { Form, FormControl, FormField, FormItem } from './ui/form'
 import { Search } from 'lucide-react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
-import { on } from 'events'
 
 const formSchema = z.object({
   searchQuery: z.string({
