24d36e3131b95122787cb4ac6d2a2b5999149c10
brucechanch <brucechanch@gmail.com>
2024-04-30 11:11:45 +0800

    Search filter and pagination done

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,47 @@
 import { useSearchRestaurants } from '@/api/RestaurantApi'
+import PaginationSelector from '@/components/PaginationSelector'
+import SearchBar, { SearchForm } from '@/components/SearchBar'
 import SearchResultCard from '@/components/SearchResultCard'
 import SearchResultInfo from '@/components/SearchResultInfo'
+// import { Pagination } from '@/components/ui/pagination'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+
+export type SearchState = {
+  searchQuery: string
+  page: number
+}
 
 const SearchPage = () => {
   const { city } = useParams()
-  const { results, isLoading } = useSearchRestaurants(city)
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: '',
+    page: 1,
+  })
+  const { results, isLoading } = useSearchRestaurants(searchState, city)
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      page,
+    }))
+  }
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+      page: 1,
+    }))
+  }
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: '',
+      page: 1,
+    }))
+  }
 
   if (isLoading) {
     ;<span>Loading ...</span>
@@ -19,10 +55,21 @@ const SearchPage = () => {
     <div className='grid gird-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
       <div id='cuisines-list'>insert cuisines here</div>
       <div id='main-content' className='flex flex-col gap-5'>
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder='Search by Cuisine or Restaurant Name'
+          onReset={resetSearch}
+        />
         <SearchResultInfo total={results.pagination.total} city={city} />
         {results.data.map((restaurant) => (
           <SearchResultCard restaurant={restaurant} />
         ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
       </div>
     </div>
   )
