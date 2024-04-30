aeb95ded88b3ce611138fb1d7ea5c0ba017951b6
brucechanch <brucechanch@gmail.com>
2024-04-30 16:18:35 +0800

    detail page and cart function

--- src/types.ts
+++ src/types.ts
@@ -8,25 +8,25 @@ export type User = {
 }
 
 export type MenuItem = {
-  _id:string;
-  name:string;
-  price:number;
+  _id: string;
+  name: string;
+  price: number;
+};
 
-}
 
 export type Restaurant = {
-  _id:string;
-  user:string;
-  restaurantName:string;
-  city:string;
-  country:string;
-  deliveryPrice:number;
-  estimatedDeliveryTime:number;
-  cuisines:string[];
-  menuItems:MenuItem[];
-  imageUrl:string;
+  _id: string;
+  user: string;
+  restaurantName: string;
+  city: string;
+  country: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  imageUrl: string;
   lastUpdated: string;
-}
+};
 
 export type RestaurantSearchResponse = {
   data: Restaurant[];
